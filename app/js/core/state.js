fa86d3a5d604417a6fdd27134288cb9b08141b25
Sean Dokko <dokko1230@gmail.com>
2014-06-26 20:44:10 -0700

    initial commit

---
+++ app/js/core/state.js
@@ -0,0 +1,25 @@
+//Application Level State
+app.config(['$stateProvider', function($stateProvider) {
+
+  $stateProvider
+    .state('app', {
+      url: '',
+      controller: 'AppCtrl',
+      views: {
+        'navbar': {
+          templateUrl: 'js/core/templates/navbar.tpl.html',
+          controller: 'NavbarCtrl'
+        },
+        'main': {
+          templateUrl: 'js/core/templates/main.tpl.html',
+          controller: 'MainCtrl'
+        }
+      }
+    })
+    .state('404', {
+      url: '/404',
+      templateUrl: 'js/core/templates/404.tpl.html',
+      controller: 'AppCtrl'
+    });
+
+}]);
