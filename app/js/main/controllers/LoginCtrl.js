d323505c4ef5d4a963af5e2f6375a1bc227369df
godspeedyoo <godspeedyoo@gmail.com>
2015-04-28 15:45:31 -0700

    store authResult in rootScope and provide alert for login error

--- app/js/main/controllers/LoginCtrl.js
+++ app/js/main/controllers/LoginCtrl.js
@@ -1,12 +1,12 @@
-app.controller('LoginCtrl', ['$rootScope','$scope', function($rootScope, $scope) {
+app.controller('LoginCtrl', ['$rootScope','$scope', '$state', function($rootScope, $scope, $state) {
 
 	$scope.authenticate = function(Oauth) {
 		OAuth.initialize('ZEezHY42tLMdO9i2rKNBAgAxdak')
 		OAuth.popup('twitter').done(function(result) {
-	    console.log(result);
-		    // do some stuff with result
+	    $rootScope.authResult = result;
+	    $state.go('app.home');
 		}).fail(function(error) {
-			console.log(error.message);
+			alert(error.message);
 		})
 	
 
