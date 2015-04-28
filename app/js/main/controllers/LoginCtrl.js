49df67a070bc44655856a07fa5118584359dbca3
godspeedyoo <godspeedyoo@gmail.com>
2015-04-28 15:38:06 -0700

    Added twitter login functionality

--- app/js/main/controllers/LoginCtrl.js
+++ app/js/main/controllers/LoginCtrl.js
@@ -1,3 +1,14 @@
-app.controller('LoginCtrl', ['$scope', function($scope) {
-	OAuth.initialize('TvTnqzZPXoOqGeAzGbRD5Zqr3');
+app.controller('LoginCtrl', ['$rootScope','$scope', function($rootScope, $scope) {
+
+	$scope.authenticate = function(Oauth) {
+		OAuth.initialize('ZEezHY42tLMdO9i2rKNBAgAxdak')
+		OAuth.popup('twitter').done(function(result) {
+	    console.log(result);
+		    // do some stuff with result
+		}).fail(function(error) {
+			console.log(error.message);
+		})
+	
+
+	}		
 }]);
