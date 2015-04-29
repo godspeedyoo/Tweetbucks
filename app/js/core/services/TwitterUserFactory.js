06fd2772aab188d51b10a77173c2baf85d6dfa45
godspeedyoo <godspeedyoo@gmail.com>
2015-04-29 01:16:42 -0700

    implement TwitterUserFactory.getFriends, i.e. people whom current user is following

--- app/js/core/services/TwitterUserFactory.js
+++ app/js/core/services/TwitterUserFactory.js
@@ -3,15 +3,21 @@
 app.factory('TwitterUserFactory', ['$rootScope', '$scope', function($rootScope, $scope) {
 	
 	var users = {};
-	var url = '/1.1/followers/list.json'
+	var followersUrl = '/1.1/followers/list.json';
+	var friendsUrl = '1.1/friends/list.json';
 
 	users.getFollowers = function() {
-		$rootScope.twitterOAuthResult.get(url).success(function(response) {
+		$rootScope.twitterOAuthResult.get(followersUrl).success(function(response) {
 			$scope.followers = response.users;
+		});
+	};
+
+	users.getFriends = function() {
+		$rootScope.twitterOAuthResult.get(friendsUrl).success(function(response) {
+			$scope.friends = response.users;
 		});
 	};
 
 	return users;
 
-
-}])
+}]);
