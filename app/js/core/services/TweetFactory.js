65c69504a5a9bf9119fb7c7c1ea00228b3e48bd2
godspeedyoo <godspeedyoo@gmail.com>
2015-04-29 17:41:25 -0700

    Apply styling for follower/friends count and apply ng-show directives for elements on home page

--- app/js/core/services/TweetFactory.js
+++ app/js/core/services/TweetFactory.js
@@ -5,7 +5,7 @@ app.factory('TweetFactory', ['$rootScope', function($rootScope) {
 	var tweets = {};
 	var postUrl = '/1.1/statuses/update.json';
 	var getUrl = '/1.1/statuses/user_timeline.json';
-	
+
 	tweets.getTweets = function() {
 		return $rootScope.twitterOAuthResult.get(getUrl);
 	};
