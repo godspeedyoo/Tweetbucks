2f19686ebffcf4868eb67b69ef2569b5774d26d4
godspeedyoo <godspeedyoo@gmail.com>
2015-04-30 15:42:21 -0700

    specify static file path

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/app'));
+app.get("/", function(req, res) {
+	res.sendfile('app/index.html');
+});
+
+app.use(express.static('bower_components'));
+
 app.listen(process.env.PORT || 3000);
