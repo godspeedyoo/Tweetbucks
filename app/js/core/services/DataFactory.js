b2810d05141e7ba87d5d6d8bb21e421548dd8ebf
godspeedyoo <godspeedyoo@gmail.com>
2015-04-30 11:23:27 -0700

    Create datafactory and add to index

---
+++ app/js/core/services/DataFactory.js
@@ -0,0 +1,19 @@
+app.factory('DataService', function($rootScope) {
+
+	var data = {};
+	data.twitterOAuthResult = {};
+	data.tweets = {};
+
+	data.updateOAuthResult = function(result) {
+		this.twitterOAuthResult = result;
+		$rootScope.$broadcast("valuesUpdated");
+	}
+
+	data.updateTweets = function(tweets) {
+		this.tweets = tweets;
+		$rootScope.$broadcast("valuesUpdated");
+	}
+
+	return data;
+
+})
