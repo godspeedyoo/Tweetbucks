e7bcdffb1bb7fd05efe32e033284624da2e85341
godspeedyoo <godspeedyoo@gmail.com>
2015-04-30 14:44:29 -0700

    Cleanup NavbarCtrl - .remove is not a function, need to implement logout

--- app/js/core/controllers/NavbarCtrl.js
+++ app/js/core/controllers/NavbarCtrl.js
@@ -1,8 +1,12 @@
-app.controller('NavbarCtrl', ['$rootScope', '$scope', function($scope) {
+app.controller('NavbarCtrl', ['$rootScope', '$scope', '$cookies', '$state',
+ function($rootScope, $scope, $cookies, $state) {
   $scope.items = ['Home', 'About', 'Contact'];
 
   $scope.logout = function() {
   	OAuth.clearCache();
-  	alert("You have logged out.");
+  	//
+  	// $cookies.remove('oauthio_provider_twitter');
+  	// alert("You have logged out.");
+  	// $state.go('app.login');
   }
 }]);
